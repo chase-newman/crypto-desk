886f0a2622e64e8b71591cfe221a8201c6e51b83
EC2 Default User <ec2-user@ip-172-31-27-81.ec2.internal>
2020-06-06 22:54:46 +0000

    add an additional row off data in css ticker for longer scrolling

--- src/Components/CSSTicker/cssTicker.js
+++ src/Components/CSSTicker/cssTicker.js
@@ -61,7 +61,7 @@ class CSSTicker extends Component {
                                         <span style={this.props.rippleDelta > 0 ? {color: "#00ff00"} : {color: "red"}}>
                                             <strong> {this.props.rippleDelta > 0 ? "+" : null} {this.props.rippleDelta}</strong>
                                         </span>
-                                    </div>
+                                </div>
                                 <div className="ticker-item">
                                     <span className="css-ticker-symbol">
                                         <strong>LTC: </strong>
@@ -94,14 +94,78 @@ class CSSTicker extends Component {
                                             <strong> {this.props.bitcoinSVDelta > 0 ? "+" : null} {this.props.bitcoinSVDelta}</strong>
                                         </span>
                                     </div>
-                                <div className="ticker-item last-item">
+                                <div className="ticker-item">
                                     <span className="css-ticker-symbol">
                                         <strong>BCH: </strong>
                                     </span> ${this.state.bitcoinCash} 
                                         <span style={this.props.bitcoinCashDelta > 0 ? {color: "#00ff00"} : {color: "red"}}>
                                             <strong> {this.props.bitcoinCashDelta > 0 ? "+" : null} {this.props.bitcoinCashDelta}</strong>
                                         </span>
+                                </div>
+                                <div className="ticker-item">
+                                    <span className="css-ticker-symbol">
+                                        <strong>BTC: </strong> 
+                                    </span> ${this.state.bitcoin} 
+                                    <span style={this.props.bitcoinDelta > 0 ? {color: "#00ff00"} : {color: "red"}}>
+                                        <strong> {this.props.bitcoinDelta > 0 ? "+" : null} {this.props.bitcoinDelta}</strong>
+                                    </span>
+                                </div>
+                                <div className="ticker-item">
+                                    <span className="css-ticker-symbol">
+                                        <strong>ETH: </strong>
+                                    </span> ${this.state.ethereum} 
+                                        <span style={this.props.ethereumDelta > 0 ? {color: "#00ff00"} : {color: "red"}}>
+                                            <strong> {this.props.ethereumDelta > 0 ? "+" : null} {this.props.ethereumDelta}</strong>
+                                        </span>
+                                </div>
+                                <div className="ticker-item">
+                                    <span className="css-ticker-symbol">
+                                        <strong>XRP: </strong> 
+                                    </span> ${this.state.ripple} 
+                                        <span style={this.props.rippleDelta > 0 ? {color: "#00ff00"} : {color: "red"}}>
+                                            <strong> {this.props.rippleDelta > 0 ? "+" : null} {this.props.rippleDelta}</strong>
+                                        </span>
+                                </div>
+                                <div className="ticker-item">
+                                    <span className="css-ticker-symbol">
+                                        <strong>LTC: </strong>
+                                    </span> ${this.state.litecoin} 
+                                        <span style={this.props.litecoinDelta > 0 ? {color: "#00ff00"} : {color: "red"}}>
+                                            <strong> {this.props.litecoinDelta > 0 ? "+" : null} {this.props.litecoinDelta}</strong>
+                                        </span>
                                     </div>
+                                <div className="ticker-item">
+                                    <span className="css-ticker-symbol">
+                                        <strong>USDT: </strong>
+                                    </span> ${this.state.tether} 
+                                        <span style={this.props.tetherDelta > 0 ? {color: "#00ff00"} : {color: "red"}}>
+                                            <strong> {this.props.tetherDelta > 0 ? "+" : null} {this.props.tetherDelta}</strong>
+                                        </span>
+                                    </div>
+                                <div className="ticker-item">
+                                    <span className="css-ticker-symbol">
+                                        <strong>EOS: </strong>
+                                    </span> ${this.state.eos} 
+                                        <span style={this.props.eosDelta > 0 ? {color: "#00ff00"} : {color: "red"}}>
+                                            <strong> {this.props.eosDelta > 0 ? "+" : null} {this.props.eosDelta}</strong>
+                                        </span>
+                                    </div>
+                                <div className="ticker-item">
+                                    <span className="css-ticker-symbol">
+                                        <strong>BSV: </strong>
+                                    </span> ${this.state.bitcoinSV} 
+                                        <span style={this.props.bitcoinSVDelta > 0 ? {color: "#00ff00"} : {color: "red"}}>
+                                            <strong> {this.props.bitcoinSVDelta > 0 ? "+" : null} {this.props.bitcoinSVDelta}</strong>
+                                        </span>
+                                    </div>
+                                <div className="ticker-item">
+                                    <span className="css-ticker-symbol">
+                                        <strong>BCH: </strong>
+                                    </span> ${this.state.bitcoinCash} 
+                                        <span style={this.props.bitcoinCashDelta > 0 ? {color: "#00ff00"} : {color: "red"}}>
+                                            <strong> {this.props.bitcoinCashDelta > 0 ? "+" : null} {this.props.bitcoinCashDelta}</strong>
+                                        </span>
+                                </div>
                             </div>
                         </div>
                       </div>
