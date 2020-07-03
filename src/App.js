d4311ab6cdf057745628d5fa0e483b7d0641182f
EC2 Default User <ec2-user@ip-172-31-27-81.ec2.internal>
2020-07-03 16:14:03 +0000

    fix bug in componentDidMount

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ class App extends Component {
               firebaseDate = firebaseDate[firebaseDate.length - 1].bitcoinCash.date;
               //Get the unique ID of the most recent date object store in firebase
               let data = Object.entries(response.data);
-              data = data.slice(0,10)
+
               let idData = data[data.length - 1];
               let id = idData[0];
     //If the dates are the same we want to update the most recent posted data in the firebase database to reflect
@@ -168,10 +168,7 @@ class App extends Component {
                               .then(response => {
                                   data = Object.entries(response.data);
                                   data = data.slice(data.length - 30,data.length + 1);
-                                  
-                                  
-                                  
-                                  
+                                
                                   
                                   let dateLabels = [];
                                   let dateFiveDay = [];
